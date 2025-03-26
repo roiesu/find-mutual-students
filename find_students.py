051125119c8a31e1857b20b231a41bc0beb934ca
roiesu <roiesu3@gmail.com>
2025-03-26 20:37:58 +0200

    add comment

--- find_students.py
+++ find_students.py
@@ -4,17 +4,20 @@ def find_students(computers_path, physics_path):
     
     computers_students = set(line.strip().lower() for line in computers)
     
+    # Created the mutual_Student list for testing, more effiecient (space comlexity) just print common students.
     mutual_students = []
     for line in physics:
         student = line.strip().lower()
         if student in computers_students:
             mutual_students.append(student)
 
+    print(mutual_students)
+    
     computers.close()
     physics.close()
+    
     return mutual_students
     
-
 def main():
     find_students()
 
